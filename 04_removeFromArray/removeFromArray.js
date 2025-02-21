0c41f97818308cfc5853b28fe7990b61db849e02
Matthew Richardson <matthew_richardson@ymail.com>
2025-02-21 23:36:16 +0000

    answer splice not work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,arg) {
+
+//newArray = array.splice(!arg,0);
+
+//test = [1,2,4]; result needed
+return newArray
 };
+
+
+//do this
+
+//for loop to go through array.length, i = 0; i < array.length; i++
+
+//then splice it with the argument and put those into a new array
+
+//return the array
+
+//probably look into splice more to understand it
+
+//try array.splice(0,arg)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
